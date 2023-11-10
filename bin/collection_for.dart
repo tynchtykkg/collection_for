e1dd54ffe728d1f30ea76645ded38c6cb18a2f76
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-10 15:34:47 +0600

    first commit

---
+++ bin/collection_for.dart
@@ -0,0 +1,5 @@
+import 'package:collection_for/collection_for.dart' as collection_for;
+
+void main(List<String> arguments) {
+  print('Hello world: ${collection_for.calculate()}!');
+}
---
+++ test/collection_for_test.dart
@@ -0,0 +1,8 @@
+import 'package:collection_for/collection_for.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
